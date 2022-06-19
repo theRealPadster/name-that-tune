37ae2ac0309c42845b35dd6a36e620d2101b6941
Isaac Maier <padspambox@gmail.com>
2022-06-19 13:40:49 -0400

    🐛 Add Button component from Marketplace to fix the button styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 
+import styles from '../css/button.module.scss';
+
+// Round is the default style
+// Circle is used by the install/remove button
+type ButtonType = 'round' | 'circle';
+
 const Button = (props: {
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  classes?: string[];
+  label?: string;
+  type?: ButtonType;
+  children: React.ReactNode;
   disabled?: boolean;
-  onClick: Function;
-  children?: React.ReactNode;
 }) => {
+  const buttonType = props.type || 'round';
+
+  const classList = [styles.button];
+  if (buttonType === 'circle') classList.push(styles.circle);
+  if (props.classes) classList.push(...props.classes);
+
   return (
-    <button
-      className="main-buttons-button main-button-secondary"
-      disabled={props.disabled}
-      onClick={props.onClick}
-    >
+    <button className={classList.join(' ')} onClick={props.onClick} aria-label={props.label} disabled={props.disabled}>
       {props.children}
     </button>
   );
